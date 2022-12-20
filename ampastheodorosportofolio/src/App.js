e15d7a48991be057487bb897c0fde1933bffd862
TheodorAmpas <ampastheodwros@gmail.com>
2022-12-21 01:34:17 +0200

    adding font icon

--- ampastheodorosportofolio/src/App.js
+++ ampastheodorosportofolio/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Contact from "./Contact";
-import Work from "./Work";
+import Experiences from "./Experiences";
+import Blog from "./Blog";
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/AmpasTheodorosPortofolio" element={<Home />} />
-          <Route path="/AmpasTheodorosPortofolio/work" element={<Work />} />
+          <Route path="/AmpasTheodorosPortofolio/experiences" element={<Experiences />} />
+          <Route path="/AmpasTheodorosPortofolio/blog" element={<Blog />} />
           <Route path="/AmpasTheodorosPortofolio/contact" element={<Contact />} />
         </Routes>
       </Router>
--- ampastheodorosportofolio/src/components/Navbar/Button.js
+++ ampastheodorosportofolio/src/components/Navbar/Button.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Button = ({ toggleNav }) => {
     return (
         <button id="nav-toggle" type="button" onClick={toggleNav} aria-label="Name">
-            <i class="open fa-light fa-bars-staggered"></i>
-            <i class="close fa-light fa-xmark-large"></i>
+            {/* <i class="open fa fa-bars" aria-hidden="true"></i> */}
+            {/* <i class="open fa fa-chevron-up" aria-hidden="true"></i> */}
+            <i class="open fa fa-align-left" aria-hidden="true"></i>
+            <i class="close fa fa-times" aria-hidden="true"></i>
         </button>
     );
 };
